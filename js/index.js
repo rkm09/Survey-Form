d660227f853baa3609b89f04bd16ba546ab579f2
rkm09 <raksmishra.raksha09@gmail.com>
2017-12-06 12:52:47 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*$(document).ready(function(){
+  $(".board").click(function(){
+   
+    $("#description").fadeOut(300);
+    $(".board").fadeOut(300);
+    $(".form1").fadeIn(1000);
+   // $("#submit").fadeIn("slow");
+   // $("#submit").fadeIn(3000);
+});
+  $("#submit").click(function(){
+    $("#go").hide();
+    $("header").hide();
+    $("footer").hide();
+    $("#hid").text("Thank You! Successfully Submitted :)");
+});
+});
+*/
